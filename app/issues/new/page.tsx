a493ecfb40a6598bd03724bc7942cd9508052e73
ScottyCompton <scott.lonis@outlook.com>
2025-07-16 09:04:57 -0700

    added markdown editory

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,10 +1,21 @@
-import React from 'react'
-import { TextField, TextArea, Button } from '@radix-ui/themes'
+'use client'
+import { TextField, Button } from '@radix-ui/themes'
+import SimpleMDE from "react-simplemde-editor";
+import "easymde/dist/easymde.min.css"; // Import the CSS for the editor
+import { useState } from 'react';
+
 const NewIssue = () => {
+    const [markdown, setMarkdown] = useState('');
+
+    const handleChange = (value:any) => {
+      setMarkdown(value);
+    };    
   return (
     <div className='max-w-xl space-y-3'>
         <TextField.Root placeholder='Title' />
-        <TextArea placeholder='Description' />
+        <div>
+            <SimpleMDE placeholder='Description' value={markdown} onChange={handleChange} />
+         </div>    
         <Button>Create Issue</Button>
     </div>
   )
