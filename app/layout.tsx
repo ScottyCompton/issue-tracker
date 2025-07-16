a184fe7287538816e3ee915cc77a48d691919967
ScottyCompton <scott.lonis@outlook.com>
2025-07-16 08:02:53 -0700

    changed theme font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,15 @@
+import "@radix-ui/themes/styles.css";
+import './theme-config.css';
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import Navbar from './components/Navbar'
-import "@radix-ui/themes/styles.css";
 import { Theme } from "@radix-ui/themes"
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({
+	subsets: ["latin"],
+	variable: "--font-inter",
+});
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -18,8 +22,8 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en" data-theme="light">
-      <body className={inter.className}>
+    <html lang="en">
+      <body className={inter.variable}>
         <Theme>
           <Navbar />
           <main className='p-5'>{children}</main>
