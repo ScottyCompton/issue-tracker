e1071ad7548094d630308850133e2773d587a97e
ScottyCompton <scott.lonis@outlook.com>
2025-07-16 09:51:08 -0700

    added create issue functionality to new issues page

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -2,22 +2,40 @@
 import { TextField, Button } from '@radix-ui/themes'
 import SimpleMDE from "react-simplemde-editor";
 import "easymde/dist/easymde.min.css"; // Import the CSS for the editor
-import { useState } from 'react';
+import { SetStateAction, useState } from 'react';
+import { useForm, SubmitHandler, Controller } from "react-hook-form"
+import axios from "axios"
+import { useRouter } from 'next/navigation';
+
+interface IssueForm  {
+    title: string,
+    description: string
+}
+
 
 const NewIssue = () => {
-    const [markdown, setMarkdown] = useState('');
+    const { register, control, handleSubmit,  formState: { errors } } = useForm<IssueForm>();
+    const router = useRouter()
 
-    const handleChange = (value:any) => {
-      setMarkdown(value);
-    };    
-  return (
-    <div className='max-w-xl space-y-3'>
-        <TextField.Root placeholder='Title' />
+    const onSubmit:SubmitHandler<IssueForm> = async (data) => {
+       await axios.post('/api/issues', data)
+       router.push('/issues')
+    }
+
+    return (
+        <form className='max-w-xl space-y-3' onSubmit={handleSubmit(onSubmit)}>
+        <TextField.Root placeholder='Title' {...register("title")} />
         <div>
-            <SimpleMDE placeholder='Description' value={markdown} onChange={handleChange} />
+            <Controller 
+                name="description"
+                control={control}
+                render={({field}) => <SimpleMDE placeholder='Description' {...field} />}
+                />
+                
+                
          </div>    
         <Button>Create Issue</Button>
-    </div>
+        </form>
   )
 }
 
