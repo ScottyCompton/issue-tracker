76fff2421c38a6737aaab58efaabb7aef454b930
ScottyCompton <scott.lonis@outlook.com>
2025-07-16 07:07:08 -0700

    setup prisma client

---
+++ prisma/client.ts
@@ -0,0 +1,16 @@
+import { PrismaClient } from "@/app/generated/prisma";
+
+const prismaClientSingleton = () => {
+    return new PrismaClient()
+}
+
+
+type prismaClientSingleton = ReturnType<typeof prismaClientSingleton>
+
+const globalForPrisma = globalThis as unknown as {
+    prisma: prismaClientSingleton | undefined
+}
+
+const prisma = globalForPrisma ?? prismaClientSingleton
+
+export default prisma
