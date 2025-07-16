5dd40073209668788dea114855c2695308c8fc5a
ScottyCompton <scott.lonis@outlook.com>
2025-07-16 04:32:00 -0700

    backgrack to more or less initial commit

--- app/users/[id]/not-found.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const UserNotFoundPage = () => {
-  return (
-    <div>The user with the given ID was not found</div>
-  )
-}
-
-export default UserNotFoundPage
--- middleware.ts
+++
@@ -1,7 +0,0 @@
-import { NextRequest, NextResponse } from "next/server";
-export { default } from "next-auth/middleware";
-
-
-export const config = {
-    matcher: ['/dashboard/:path*']
-}
