af0318232a0b38d56c5089362063839979cfd60c
ScottyCompton <scott.lonis@outlook.com>
2025-07-16 07:34:25 -0700

    added create issue basic page

---
+++ app/issues/new/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { TextField, TextArea, Button } from '@radix-ui/themes'
+const NewIssue = () => {
+  return (
+    <div className='max-w-xl space-y-3'>
+        <TextField.Root placeholder='Title' />
+        <TextArea placeholder='Description' />
+        <Button>Create Issue</Button>
+    </div>
+  )
+}
+
+export default NewIssue
