6f8415bb7ca4ced7612dd6f9ebc7f3747a70aa62
ScottyCompton <scott.lonis@outlook.com>
2025-07-18 06:33:10 -0700

    add loading skeleton to issues list view

---
+++ app/issues/loading.tsx
@@ -0,0 +1,34 @@
+import { Skeleton, Table } from '@radix-ui/themes'
+import React from 'react'
+import IssuesToolbar from './components/IssuesToolbar'
+
+const IssuesLoadingSkeleton = () => {
+  const issues = [1,2,3,4,5]
+
+  return (
+    <>
+    <IssuesToolbar loading={true} />
+    <Table.Root variant='surface'>
+                <Table.Header>
+                    <Table.Row>
+                    <Table.ColumnHeaderCell>Issue</Table.ColumnHeaderCell>
+                    <Table.ColumnHeaderCell className='hidden md:table-cell'>Status</Table.ColumnHeaderCell>
+                    <Table.ColumnHeaderCell className='hidden md:table-cell'>Created</Table.ColumnHeaderCell>
+                    </Table.Row>
+                </Table.Header>
+                <Table.Body>
+                    {issues.map((issue) => (
+                        <Table.Row key={issue}>
+                            <Table.Cell><Skeleton /><div className='block md:hidden'><Skeleton /></div></Table.Cell>
+                            <Table.Cell className='hidden md:table-cell'><Skeleton /></Table.Cell>
+                            <Table.Cell className='hidden md:table-cell'><Skeleton /></Table.Cell>
+                        </Table.Row>
+                    ))}
+
+                </Table.Body>
+            </Table.Root>
+     </>
+  )
+}
+
+export default IssuesLoadingSkeleton
--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -3,34 +3,18 @@ import { Button, Table } from '@radix-ui/themes'
 import Link from 'next/link'
 import prisma from '@/prisma/client'
 import IssueStatusBadge from '../components/IssueStatusBadge'
+import delay from 'delay'
+import IssuesToolbar from './components/IssuesToolbar'
+import IssuesList from './components/IssuesList'
 
 const IssuesPage = async () => {
   
   const issues = await prisma.issue.findMany()
-
+  await delay(2000)
   return (
     <div>
-        <div className='mb-5 text-right'><Button><Link href='/issues/new/'>Create Issue</Link></Button></div>
-        {issues.length > 0 && 
-        <Table.Root variant='surface'>
-                <Table.Header>
-                    <Table.Row>
-                    <Table.ColumnHeaderCell>Issue</Table.ColumnHeaderCell>
-                    <Table.ColumnHeaderCell className='hidden md:table-cell'>Status</Table.ColumnHeaderCell>
-                    <Table.ColumnHeaderCell className='hidden md:table-cell'>Created</Table.ColumnHeaderCell>
-                    </Table.Row>
-                </Table.Header>
-                <Table.Body>
-                    {issues.map((issue) => (
-                        <Table.Row key={issue.id}>
-                            <Table.Cell>{issue.title}<div className='block md:hidden'><IssueStatusBadge status={issue.status} /></div></Table.Cell>
-                            <Table.Cell className='hidden md:table-cell'><IssueStatusBadge status={issue.status} /></Table.Cell>
-                            <Table.Cell className='hidden md:table-cell'>{issue.createdAt.toDateString()}</Table.Cell>
-                        </Table.Row>
-                    ))}
-
-                </Table.Body>
-            </Table.Root>}
+        <IssuesToolbar />
+        <IssuesList />
     </div>
   )
 }
