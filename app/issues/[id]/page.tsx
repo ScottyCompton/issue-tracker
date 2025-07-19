66a27ca64cd9c91c055d250a2cfad82755eb7b44
ScottyCompton <scott.lonis@outlook.com>
2025-07-19 06:56:40 -0700

    refactor Issue Details Page

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -1,10 +1,8 @@
-import { IssueStatusBadge } from '@/app/components'
 import prisma from '@/prisma/client'
-import { Box, Button, Card, Flex, Grid, Heading } from '@radix-ui/themes'
+import { Box, Grid } from '@radix-ui/themes'
 import { notFound } from 'next/navigation'
-import ReactMarkdown from 'react-markdown'
-import { Pencil2Icon } from '@radix-ui/react-icons'
-import Link from 'next/link'
+import EditIssueButton from './EditIssueButton'
+import IssueDetails from './IssueDetails'
 
 interface Props {
     params: Promise<{
@@ -12,34 +10,24 @@ interface Props {
     }>
 }
 
-
-const IssueDetailsPage:React.FC<Props> = async ({params}: Props) => {
-    const {id} = await params
+const IssueDetailsPage: React.FC<Props> = async ({ params }: Props) => {
+    const { id } = await params
 
     const issue = await prisma.issue.findUnique({
-        where: {id: parseInt(id)}
+        where: { id: parseInt(id) },
     })
 
-    if(!issue) notFound()
-  return (
-    <Grid columns={{initial: "1", md: "2"}} gap="5">
-        <Box>
-        <Heading>{issue.title}</Heading>
-        <Flex className='space-x-3' my="2">
-            <p><IssueStatusBadge status={issue.status} /></p>
-            <p>{issue.createdAt.toDateString()}</p>
-        </Flex>
-        <Card mt="4">
-            <span className='prose'>
-                <ReactMarkdown>{issue.description}</ReactMarkdown>
-            </span>
-        </Card>
-        </Box>
-        <Box>
-            <Button><Pencil2Icon /><Link href={`/issues/${id}/edit`}>Edit Issue</Link></Button>
-        </Box>
-    </Grid>
-  )
+    if (!issue) notFound()
+    return (
+        <Grid columns={{ initial: '1', md: '2' }} gap="5">
+            <Box>
+                <IssueDetails issue={issue} />
+            </Box>
+            <Box>
+                <EditIssueButton issueId={id} />
+            </Box>
+        </Grid>
+    )
 }
 
 export default IssueDetailsPage
