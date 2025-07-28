1a54911b80e16de7f8528d6674084ce05678159a
ScottyCompton <scott.lonis@outlook.com>
2025-07-28 13:05:04 -0700

    added type definitions for LatestIssues

--- app/components/LatestIssues.tsx
+++ app/components/LatestIssues.tsx
@@ -1,12 +1,30 @@
 import { client as graphqlClient } from '@/app/lib/graphql-client'
-import { GET_LATEST_ISSUES_QUERY } from '../graphql/queries'
+import { Status } from '@/prisma/client'
 import { Avatar, Card, Flex, Heading, Table } from '@radix-ui/themes'
-import { Issue } from '@/app/lib/interfaces'
 import Link from 'next/link'
+import { GET_LATEST_ISSUES_QUERY } from '../graphql/queries'
 import IssueStatusBadge from './IssueStatusBadge'
 
+interface AssignedToUser {
+    id: string
+    name: string
+    email: string
+    image: string
+}
+
+interface LatestIssue {
+    id: string
+    title: string
+    status: Status
+    assignedToUser: AssignedToUser
+}
+
+interface LatestIssues {
+    latestIssues: LatestIssue[]
+}
+
 const LatestIssues: React.FC = async () => {
-    const { data } = await graphqlClient.query({
+    const { data } = await graphqlClient.query<LatestIssues>({
         query: GET_LATEST_ISSUES_QUERY,
     })
 
@@ -19,37 +37,40 @@ const LatestIssues: React.FC = async () => {
             </Heading>
             <Table.Root>
                 <Table.Body>
-                    {latestIssues.map((issue: Issue) => (
-                        <Table.Row key={issue.id}>
-                            <Table.Cell>
-                                <Flex justify="between">
-                                    <Flex
-                                        direction="column"
-                                        align="start"
-                                        gap="2"
-                                    >
-                                        <Link
-                                            href={`/issues/${issue.id}`}
-                                            className="hover:text-violet-600 transition-colors"
+                    {latestIssues &&
+                        latestIssues.map((issue: LatestIssue) => (
+                            <Table.Row key={issue.id}>
+                                <Table.Cell>
+                                    <Flex justify="between">
+                                        <Flex
+                                            direction="column"
+                                            align="start"
+                                            gap="2"
                                         >
-                                            {issue.title}
-                                        </Link>
-                                        <IssueStatusBadge
-                                            status={issue.status}
-                                        />
+                                            <Link
+                                                href={`/issues/${issue.id}`}
+                                                className="hover:text-violet-600 transition-colors"
+                                            >
+                                                {issue.title}
+                                            </Link>
+                                            <IssueStatusBadge
+                                                status={issue.status}
+                                            />
+                                        </Flex>
+                                        {issue.assignedToUser && (
+                                            <Avatar
+                                                src={
+                                                    issue.assignedToUser.image!
+                                                }
+                                                fallback="?"
+                                                size="2"
+                                                radius="full"
+                                            />
+                                        )}
                                     </Flex>
-                                    {issue.assignedToUser && (
-                                        <Avatar
-                                            src={issue.assignedToUser.image!}
-                                            fallback="?"
-                                            size="2"
-                                            radius="full"
-                                        />
-                                    )}
-                                </Flex>
-                            </Table.Cell>
-                        </Table.Row>
-                    ))}
+                                </Table.Cell>
+                            </Table.Row>
+                        ))}
                 </Table.Body>
             </Table.Root>
         </Card>
